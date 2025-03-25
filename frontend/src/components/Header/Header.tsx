7d8d8477c476318be8a8bf57d66b40a728ca3d19
mousearthouse <mousearthousee@gmail.com>
2025-03-25 20:22:44 +0300

    Fix navigation

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import './header.scss';
+import { useNavigate } from 'react-router-dom';
 import mouse from '/assets/mouse.svg';
 
 const Header: React.FC = () => {
+    const navigate = useNavigate();
+
+    const openFavoritesPage = () => {
+      navigate(`/favorites`);
+    };
+
+    const openRootPage = () => {
+        navigate(`/`);
+      };
+
     return (
         <header className="header_navigation">
             <div className='header_navigation_content'>
                 <div className="header_left">
                     <img src={mouse} alt="Logo" className="header_icon" />
-                    <a className="site-name" href="/">МЫШКИН ШОП</a>
+                    <a className="site-name" onClick={() => openRootPage()}>МЫШКИН ШОП</a>
                 </div>
                 <nav className="header_center">
-                    <a href="/favorites" className='header'>
+                    <a onClick={() => openFavoritesPage()} className='header'>
                         Избранное
                     </a>
                 </nav>                
